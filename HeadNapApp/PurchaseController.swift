d7216fd1f4e35ed7ddf65f91910350955b4b3b0c
Jan Brinkmann <lucky@the-luckyduck.de>
2016-05-12 17:36:28 +0200

    fixed crash

--- HeadNapApp/PurchaseController.swift
+++ HeadNapApp/PurchaseController.swift
@@ -26,8 +26,8 @@ class PurchaseController: UIViewController {
     @IBOutlet weak var buyHeadNapPaket: UIButton!
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
         
         labelVideoPaket.text = "\(paketName!)"
         
@@ -89,6 +89,7 @@ extension PurchaseController: SKPaymentTransactionObserver {
             case SKPaymentTransactionState.Failed:
                 print("nicht erfolgreich")
                 SKPaymentQueue.defaultQueue().finishTransaction(currentTransaction)
+                SKPaymentQueue.defaultQueue().removeTransactionObserver(self)
                 transactionForProductId = nil
                 
             default:
